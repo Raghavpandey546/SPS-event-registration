38c84606d24f6a2a27b6ceb61b14175e831be6d7
Raghavpandey546 <raghavpandey546@gmail.com>
2026-04-14 21:12:50 +0530

    fix bug

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
     apiKey: "AIzaSyBM883InnPvRNcJ8nexsCwMB7s07azwBv8",
     authDomain: "sps-event.firebaseapp.com",
+    databaseURL: "https://sps-event-default-rtdb.firebaseio.com", /* ADD THIS LINE */
     projectId: "sps-event",
     storageBucket: "sps-event.firebasestorage.app",
     messagingSenderId: "458242711775",
