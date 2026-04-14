f3e2298a748c1c84e9f2002a876ddae5d9d502a6
Raghavpandey546 <raghavpandey546@gmail.com>
2026-04-14 21:06:15 +0530

    fix: add firebase config so script.js can load

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBM883InnPvRNcJ8nexsCwMB7s07azwBv8",
+    authDomain: "sps-event.firebaseapp.com",
+    projectId: "sps-event",
+    storageBucket: "sps-event.firebasestorage.app",
+    messagingSenderId: "458242711775",
+    appId: "1:458242711775:web:c0cd304c70f119561c896a",
+    measurementId: "G-8C1DF9MKWP"
+  };
